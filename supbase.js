075c4978ba77bcfc0f35c106f1fc9bcb769d1ac4
aaditya-pro <121468299+aaditya-pro@users.noreply.github.com>
2026-08-26 21:14:22 +0545

    Initialize Supabase client with URL and key

---
+++ supbase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://zgzgcuhjoxhbkmqulmuu.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_bbzAVuYi8s1RMUORhuUoVw_a-u_bHuP";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+const STORAGE_BUCKET = "notes";
